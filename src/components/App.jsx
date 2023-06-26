52c1c8466e054ec8d84f31cd8ada86e9307bec30
Dimekhin Maksym <dimehin-makc@i.ua>
2023-06-27 00:58:36 +0100

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+import { PhoneBook } from "./PhoneBook/PhoneBook";
+import { nanoid } from 'nanoid'
+
+export class App extends Component{
+  state = {
+    contacts: [],
+    name: ''
+  }
+  addContact = (event) => {
+    const idNew = nanoid()
+    event.preventDefault()
+    this.setState(prevState => ({
+      name: event.target[0].value,
+      contacts: [
+        ...prevState.contacts,
+        {
+          name: event.target[0].value,
+          id: idNew
+        }
+      ]
+    }))
+  }
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.addContact}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              style={{display: "block", marginTop: 10, marginBottom:20}}
+            />
+          </label>
+          <button type="submit" >Add Contact</button>
+        </form>
+       
+        <PhoneBook/>
+      </div>
+    )
+  }
+}
+
---
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+export class PhoneBook extends Component{
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}
