514ca765b081356d3ee0099d042610261e9a1476
Dimekhin Maksym <dimehin-makc@i.ua>
2023-06-27 11:23:45 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,20 +8,26 @@ export class App extends Component{
     name: ''
   }
   addContact = (event) => {
+    console.log(event)
+    console.log(event.target)
     const idNew = nanoid()
     event.preventDefault()
     this.setState(prevState => ({
       name: event.target[0].value,
+      phone: event.target[1].value,
       contacts: [
         ...prevState.contacts,
         {
           name: event.target[0].value,
+          phone: event.target[1].value,
           id: idNew
         }
       ]
     }))
   }
   render(){
+    
+    console.log(this.state)
     return(
       <div>
         <form onSubmit={this.addContact}>
@@ -36,6 +42,17 @@ export class App extends Component{
               style={{display: "block", marginTop: 10, marginBottom:20}}
             />
           </label>
+            <label>
+              Number
+            <input
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                style={{display: "block", marginBottom:20}}
+              />
+            </label>
           <button type="submit" >Add Contact</button>
         </form>
        
